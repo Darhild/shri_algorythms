f1d27648528831e8a685f040cfe692135a01a6eb
Maria Isaeva <isaeva-mariya@yandex.ru>
2019-11-08 16:48:52 +0300

    task1

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+
+
 function bubbleSort(arr) {
     for(i = 0; i < arr.length; i++) {
-        if (arr[i + 1] > arr[i]) {
-            [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+        for(j = 0; j < arr.length; j++) {
+            if (arr[j + 1] < arr[j]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }        
         }
     }
 
@@ -10,3 +14,6 @@ function bubbleSort(arr) {
 
 const arr1 = [5, 1, 4, 2];
 console.log(bubbleSort(arr1));
+
+const arr2 = [8, 7, 6, 4];
+console.log(bubbleSort(arr2));
